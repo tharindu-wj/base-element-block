d2c073ec4cecc0fcff551534e50a14ff208a60b7
Tharindu Wickramasinghe <jhtw99@gmail.com>
2019-01-14 15:55:46 +0530

    data attr adding field

--- options.php
+++ options.php
@@ -1,30 +1,41 @@
-<?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
+<?php if (!defined('FW')) {
+    die('Forbidden');
 }
 
 $options = array(
-	'title' => array(
-		'type' => 'text',
-		'label' => __('Element Title', 'fw'),
-		'desc' => __('Set the Element title', 'fw')
-	),
-    'content' => array(
-        'type'   => 'wp-editor',
-        'label'  => __( 'Content Area', 'fw' ),
-        'desc'   => __( 'Add content for element block content area.', 'fw' ),
+    '_txt_title' => array(
+        'type' => 'text',
+        'label' => __('Element Title', 'fw'),
+        'desc' => __('Set the Element title', 'fw')
+    ),
+    '_sc_content' => array(
+        'type' => 'wp-editor',
+        'label' => __('Content Area', 'fw'),
+        'desc' => __('Add content for element block content area.', 'fw'),
         'size' => 'large',
         'wpautop' => false,
+        'editor_height' => 400,
         'shortcodes' => true
     ),
-    'slide_image' => array(
+    '_img_slide_image' => array(
         'type' => 'upload',
         'label' => __('Image', '{domain}'),
         'desc' => __('Add element block image from here', '{domain}'),
-         'images_only' => true
+        'images_only' => true
     ),
-	'css_class' => array(
-		'type' => 'text',
-		'label' => __('CSS Class', 'fw'),
-		'desc' => __('Add class to widget', 'fw')
-	),
+    'css_class' => array(
+        'type' => 'text',
+        'label' => __('CSS Class', 'fw'),
+        'desc' => __('Add class to widget', 'fw')
+    ),
+    'data_attr' => array(
+        'type' => 'addable-box',
+        'label' => __('Data Attributes', '{domain}'),
+        'desc' => __('Data Attributes', '{domain}'),
+        'template' => '{{- attribute }}',
+        'box-options' => array(
+            'attribute' => array( 'type' => 'text' ),
+            'value' => array( 'type' => 'text' ),
+        ),
+    )
 );
--- views/view.php
+++ views/view.php
@@ -1,18 +1,28 @@
 <?php
-$img_url = getImageData($atts['slide_image']);
+$img_url = getImageData($atts['_img_slide_image']);
 $css_class = $atts['css_class'];
-//fw_print($atts)
+$data_attributes =  $atts['data_attr'];
+$data_attr_str = ' ';
+
+/**
+ *
+ */
+foreach ($data_attributes as $data_attribute){
+    $data_attr_str .= $data_attribute['attribute'];
+    $data_attr_str .= "='".$data_attribute['attribute']."' ";
+}
+//fw_print($data_attr_str)
 ?>
 
-<div class="base-element-wrapper<?php if(!empty($css_class)){echo " ".$css_class;} ?>">
+<div class="base-element-wrapper<?php if(!empty($css_class)){echo " ".$css_class;} ?>" <?php echo $data_attr_str; ?>>
     <div class="base-element-container">
         <div class="base-element__thumbnail">
             <img src="<?php echo $img_url; ?>" alt="element-thumb"/>
         </div>
         <div class="base-element__content">
-            <h3 class="base-element__content__title"><?php echo $atts['title']; ?></h3>
+            <h3 class="base-element__content__title"><?php echo $atts['_txt_title']; ?></h3>
             <div class="base-element__content__body">
-                <?php echo do_shortcode($atts['content']); ?>
+                <?php echo do_shortcode($atts['_sc_content']); ?>
             </div>
         </div>
     </div>
